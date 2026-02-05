919fde6fcbfc75a7cecb45dec20f973be36a4e8f
iamKunaaal <kunaldarji2004@gmail.com>
2026-02-05 17:35:49 +0530

    fix: Use img tag for external SVG icons

--- src/components/common/Skill.tsx
+++ src/components/common/Skill.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'next-view-transitions';
-import Image from 'next/image';
 import React from 'react';
 
 interface SkillProps {
@@ -16,7 +15,8 @@ export default function Skill({ name, href, icon }: SkillProps) {
       className="skill-inner-shadow inline-flex items-center self-end rounded-md border border-dashed border-black/20 bg-black/5 px-2 py-1 text-sm text-black dark:border-white/30 dark:bg-white/15 dark:text-white"
     >
       <div className="relative size-4 flex-shrink-0">
-        <Image src={icon} alt={name} fill className="object-contain" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={icon} alt={name} className="h-full w-full object-contain" />
       </div>
       <p className="ml-1 text-sm font-bold">{name}</p>
     </Link>
